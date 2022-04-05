9dfb5dbfb1cfc68ad8c2de35158b47baccea001f
cocoludo <ludovic.lebris@gmail.com>
2022-04-05 15:15:30 +0200

    Adding singularities calculations

--- api.js
+++ api.js
@@ -2,23 +2,37 @@ const air_density = 1.2059;
 const air_viscosity = 0.000015081;
 const roughness = 0.000010;
 const calculate = document.getElementById("calculate");
+const singularities = {
+    "90_elbow": 0.4,
+    "60_elbow": 0.31,
+    "45_elbow": 0.23,
+    "30_elbow": 0.17,
+    "90_sep_tee": 2.0,
+    "90_junc_tee": 2.27,
+    "45_sep_tee": 0.58,
+    "45_junc_tee":1.64,
+    "long_reducer": 0.59,
+    "pressed_reducer": 0.35
+}
 
 calculate.addEventListener("click", function(e) {
     // e.preventDefault();
     // e.stopPropagation();
     const Flow_rate = document.getElementById("flow_rate").value;
     const Diameter = document.getElementById("diameter").value;
-    const Length = document.getElementById("length").value;
-    console.log(Flow_rate);
-    console.log(Diameter);
     const Flow_speed = flow_speed(Flow_rate, Diameter);
     document
         .getElementById("flow_speed")
         .textContent = Flow_speed;
-    const Linear_apd = linear_apd(Length, Diameter, Flow_speed);
+    const Linear_apd = linear_apd(Diameter, Flow_speed);
     document
         .getElementById("linear_apd")
         .textContent = Linear_apd;
+    for (let sing in singularities) {
+        document
+            .getElementById(sing)
+            .textContent = singular_apd(singularities[sing], Flow_speed);
+    }
 })
 
 function flow_speed(flow_rate, diameter) {
@@ -26,9 +40,8 @@ function flow_speed(flow_rate, diameter) {
     return ((flow_rate / 3600) / section).toFixed(2);
 }
 
-function linear_apd(length, diameter, flow_speed) {
+function linear_apd(diameter, flow_speed) {
     const reynolds = (flow_speed * (diameter * 10 ** -3)) / air_viscosity;
-    console.log(reynolds);
     const b = 2.51 / reynolds;
     let roughness_lambda = (1 / (-2 * Math.log10(roughness))) ** 2;
     let i = 0
@@ -42,10 +55,11 @@ function linear_apd(length, diameter, flow_speed) {
     for (let j = 0; j < lambda_list.length; j++) {
         lambda_list2.push(lambda_list[j] - roughness_lambda);
     }
-    console.log(lambda_list2);
     let min_lambda2 = Math.min.apply(Math, lambda_list2.slice(2));
-    console.log(min_lambda2);
     let lambda_def = lambda_list[lambda_list2.slice(0,6).indexOf(min_lambda2)];
-    console.log(lambda_def);
-    return ((lambda_def * length * air_density * (flow_speed ** 2)) / (2 * (diameter / 1000))).toFixed(2);
+    return ((lambda_def * air_density * (flow_speed ** 2)) / (2 * (diameter / 1000))).toFixed(2);
+}
+
+function singular_apd(singularity, flow_speed) {
+    return (singularity * air_density * (flow_speed ** 2)/2).toFixed(2);
 }
